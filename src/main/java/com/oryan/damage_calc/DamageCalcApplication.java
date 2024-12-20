3210ba8a698f9f16783efb90b7da16f7b2618f2a
lryan13 <oryanlacour@gmail.com>
2024-12-20 14:11:41 -0600

    initial commit

---
+++ src/main/java/com/oryan/damage_calc/DamageCalcApplication.java
@@ -0,0 +1,13 @@
+package com.oryan.damage_calc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamageCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DamageCalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oryan/damage_calc/DamageCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oryan.damage_calc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DamageCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
